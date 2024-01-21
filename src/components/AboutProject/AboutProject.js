72e11daf55437153efa175342f68e7dd1144b8d7
Sergey Fedorov <sergey14fedorov@gmail.com>
2024-01-21 17:50:18 +0530

    adaptive_320

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -4,24 +4,26 @@ function AboutProject() {
   return (
     <div className="about-project">
       <h2 className="about-project__header">О проекте</h2>
-      <div className="about-project__grid">
-        {/* тут грид 2х2 */}
+      <div className="about-project__container">
+        <div className="about-project__description">
+          <h3 className="about-project__description-title">
+            Дипломный проект включал 5 этапов
+          </h3>
+          <p className="about-project__description-subtitle">
+            Составление плана, работу над бэкендом, вёрстку, добавление
+            функциональности и финальные доработки.
+          </p>
+        </div>
+        <div className="about-project__description">
+          <h3 className="about-project__description-title">
+            На выполнение диплома ушло 5 недель
+          </h3>
 
-        <h3 className="about-project__description-title">
-          Дипломный проект включал 5 этапов
-        </h3>
-
-        <h3 className="about-project__description-title">
-          На выполнение диплома ушло 5 недель
-        </h3>
-        <p className="about-project__description-subtitle">
-          Составление плана, работу над бэкендом, вёрстку, добавление
-          функциональности и финальные доработки.
-        </p>
-        <p className="about-project__description-subtitle">
-          У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
-          соблюдать, чтобы успешно защититься.
-        </p>
+          <p className="about-project__description-subtitle">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+            соблюдать, чтобы успешно защититься.
+          </p>
+        </div>
       </div>
       <div className="about-project__deadline">
         {/* тут тоже грид 2х2 (1неделя, 4 недели, бекенд, фронтенд) */}
