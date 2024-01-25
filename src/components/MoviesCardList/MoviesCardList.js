02ff2d66952b8184d8c7d0d33c78fdc96c2f0b35
Sergey Fedorov <sergey14fedorov@gmail.com>
2024-01-25 22:57:26 +0530

    movies

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -17,67 +17,6 @@ function MovieCardList({ movies }) {
             />
           );
         })}
-        {/* <MovieCard
-          title={"33 слова о дизайне"}
-          image={require("../../images/33слова.png")}
-          duration={"1ч 17м"}
-          isLiked={true}
-        />
-        <MovieCard
-          title={"Киноальманах «100 лет дизайна»"}
-          image={require("../../images/100.png")}
-          duration={"1ч 17м"}
-        />
-        <MovieCard
-          title={"В погоне за Бенкси "}
-          image={require("../../images/бенкси.png")}
-          duration={"1ч 17м"}
-        />
-        <MovieCard
-          title={"Баския: Взрыв реальности"}
-          image={require("../../images/баския.png")}
-          duration={"1ч 17м"}
-        />
-        <MovieCard
-          title={"Бег это свобода "}
-          image={require("../../images/бег.png")}
-          duration={"1ч 17м"}
-        />
-        <MovieCard
-          title={"Книготорговцы "}
-          image={require("../../images/книготворцы.png")}
-          duration={"1ч 17м"}
-        />
-        <MovieCard
-          title={"Когда я думаю о Германии ночью"}
-          image={require("../../images/когда я.png")}
-          duration={"1ч 17м"}
-        />
-        <MovieCard
-          title={"Gimme Danger: История Игги и The Stooges"}
-          image={require("../../images/Gimme Dange.png")}
-          duration={"1ч 17м"}
-        />
-        <MovieCard
-          title={"Дженис: Маленькая девочка грустит"}
-          image={require("../../images/дженис.png")}
-          duration={"1ч 17м"}
-        />
-        <MovieCard
-          title={"Соберись перед прыжком"}
-          image={require("../../images/соберись.png")}
-          duration={"1ч 17м"}
-        />
-        <MovieCard
-          title={"Пи Джей Харви: A dog called money"}
-          image={require("../../images/пи джей.png")}
-          duration={"1ч 17м"}
-        />
-        <MovieCard
-          title={"По волнам: Искусство звука в кино"}
-          image={require("../../images/по волнам.png")}
-          duration={"1ч 17м"}
-        /> */}
       </section>
       <div className="movies-cardlist__button-container">
         <button className="movies-cardlist__button">Ещё</button>
